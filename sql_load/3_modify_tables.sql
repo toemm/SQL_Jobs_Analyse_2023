21d893c192353ed351251e8d21cc20f8860a2dc7
ivan <raawow@googlemail.com>
2025-08-25 13:43:28 +0200

    first commit

---
+++ sql_load/3_modify_tables.sql
@@ -0,0 +1,12 @@
+/*
+
+\copy company_dim FROM 'C:\Users\raawo\OneDrive\Programmieren\SQL\csv_files\company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+\copy skills_dim FROM 'C:\Users\raawo\OneDrive\Programmieren\SQL\csv_files\skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+\copy job_postings_fact FROM 'C:\Users\raawo\OneDrive\Programmieren\SQL\csv_files\job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+\copy skills_job_dim FROM 'C:\Users\raawo\OneDrive\Programmieren\SQL\csv_files\skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+*/
+
